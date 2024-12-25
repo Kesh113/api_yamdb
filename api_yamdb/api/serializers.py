8b6ff85c5e40f25cc92185871779cda6c58afe8b
admin <karpovv1994@icloud.com>
2024-12-25 13:16:41 +0500

    sergey+stepan

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,61 @@
+from rest_framework import serializers
+
+from reviews.models import Review, Comment, Category, Genre, Title
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['name', 'slug']
+
+
+class GenreSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Genre
+        fields = ['name', 'slug']
+
+
+class TitleSerializer(serializers.ModelSerializer):
+    rating = serializers.IntegerField(source='reviews__score__avg')
+    genre = serializers.SlugRelatedField(
+        many=True,
+        slug_field='slug',
+        queryset=Genre.objects.all()
+    )
+    category = serializers.SlugRelatedField(
+        slug_field='slug',
+        queryset=Category.objects.all()
+    )
+
+    class Meta:
+        model = Title
+        fields = ['id', 'name', 'year', 'rating',
+                  'description', 'genre', 'category']
+
+class ReviewSerializer(serializers.ModelSerializer):
+
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+    def validate(self, data):
+        if self.context.get('request').method == 'POST':
+            author = self.context.get('request').user
+            title_id = self.context.get('view').kwargs.get('title_id')
+            if Review.objects.filter(author=author, title=title_id).exists():
+                raise serializers.ValidationError(
+                    'Можно оставить только один отзыв на произведение'
+                )
+            return data
+        return data
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    author = serializers.StringRelatedField(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = '__all__'
